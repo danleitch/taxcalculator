45ca5f41c5a5335841ab2a96e45e9801062f1191
Daniel Leitch <dndleitch@gmail.com>
2021-08-11 19:04:13 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,101 @@
-import React from "react";
+import React, { useState } from 'react';
 import './App.css';
-import Tax from './components/tax_logic.jsx'
-
-
+import Tax from './components/tax_logic.jsx';
 
 function App() {
+  const [count, setCount] = useState(null);
+  const [taxBracket, setTaxBracket] = useState(null);
+
+  const inputHandler = (e) => {
+    const rawInputData = e.target.value;
+    taxCalcs(rawInputData);
+  };
+
+  // let amount = '';
+
+  const taxCalcs = (amount) => {
+    let totalTaxPayment = '';
+    // Tax Rates Based on 2022 rates.
+
+    // 18%
+    if (amount > 1 && amount < 216200) {
+      totalTaxPayment = amount * 0.18;
+      setTaxBracket('18%');
+    }
+    // 26%
+    if (amount > 216201 && amount < 337800) {
+      totalTaxPayment = amount - 216200 * 0.26 + 38916;
+      setTaxBracket('26%');
+    }
+    // 31%
+    if (amount > 337801 && amount < 467500) {
+      totalTaxPayment = amount - 337800 * 0.31 + 70532;
+      setTaxBracket('31%');
+    }
+    // 36%
+    if (amount > 467501 && amount < 613600) {
+      totalTaxPayment = amount - 467500 * 0.36 + 110739;
+      setTaxBracket('36%');
+    }
+    // 39%
+    if (amount > 613601 && amount < 782200) {
+      totalTaxPayment = amount - 613600 * 0.39 + 163335;
+      setTaxBracket('39%');
+    }
+    // 41%
+    if (amount > 782201 && amount < 1656600) {
+      totalTaxPayment = amount - 782200 * 0.41 + 229089;
+      setTaxBracket('41%');
+    }
+    // 45%
+    if (amount > 1656601) {
+      totalTaxPayment = amount * 0.45 + 587593;
+      setTaxBracket('45%');
+    }
+
+    if (buttonState) {
+      setCount(totalTaxPayment / 12);
+    }
+    if (!buttonState) {
+      setCount(totalTaxPayment);
+    }
+  };
+
+  let buttonState = true;
+
+  const buttonHandler = (e) => {
+    buttonState = !buttonState;
+    setCount(count / 12);
+    console.log(count);
+    e.preventDefault();
+  };
+
   return (
     <div>
       <header>
         <h1>Income Tax Calculator</h1>
       </header>
-
-      <p>Please enter the </p>
-      <Tax/>
-
+      <Tax />
+      {!count && <h1>Enter Income and Submit </h1>}
+      {taxBracket && <h2>Your Tax Bracket is</h2>}
+      <h2>{taxBracket}</h2>
+      {count && (
+        <h2>
+          Tax due <br /> R{count.toFixed(2)}
+        </h2>
+      )}
       <form>
-        {/* Input Amount Box */}
-        <input type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
-          <i>Submit</i>
+        <input
+          onChange={inputHandler}
+          type="number"
+          className="todo-input"
+          placeholder="Amount"
+        />
+
+        <button className="todo-button" type="submit" onClick={buttonHandler}>
+          {buttonState ? <i>Annual</i> : <i>Monthly</i>}
         </button>
-
-        <div className="select">
-          <select name="todos" className="filter-todo">
-            <option value="all">All</option>
-            <option value="completed">Monthly</option>
-            <option value="uncompleted">Annual</option>
-            
-          </select>
-        </div>
       </form>
-
-
     </div>
   );
 }
--- src/components/tax_logic.jsx
+++ src/components/tax_logic.jsx
@@ -4,58 +4,55 @@ export default function Tax() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
-  // 1 – 216 200	18% of taxable income
-  // 216 201 – 337 800	38 916 + 26% of taxable income above 216 200
-  // 337 801 – 467 500	70 532 + 31% of taxable income above 337 800
-  // 467 501 – 613 600	110 739 + 36% of taxable income above 467 500
-  // 613 601 – 782200	163335 + 39% of taxable income above 613 600
-  // 782 201 – 1656600	229089  + 41% of taxable income above 782 200
-  // 1 656 601 and above	587 593 + 45% of taxable income above 1 656 600
 
 const amount = '';
 let totalTaxPayment = '';
 
-// 18%
-if(amount > 1 && amount < 216200) {
-  totalTaxPayment = amount * 0.18 
-}
-// 26%
-if(amount > 216201 && amount < 337800){
-  totalTaxPayment = amount - 216200 * 0.18 + 38916 
-}
-// 31%
-if(amount > 337801 && amount < 467500){
-  totalTaxPayment = amount - 337800 * 0.31 + 70532 
-}
-// 36%
-if(amount > 467501 && amount < 613600){
-  totalTaxPayment = amount - 467500 * 0.36 + 110739 
-}
-// 39%
-if(amount > 613601 && amount < 782200){
-  totalTaxPayment = amount - 613600 * 0.39 + 163335 
-}
-// 41%
-if(amount > 782201 && amount < 1656600){
-  totalTaxPayment = amount - 782200 * 0.41 + 229089 
-}
-// 45%
-if(amount > 1656601){
-  totalTaxPayment = amount * 0.45 + 587593 
-}
+const taxCalcs = (inputAmount) => {
+
+  // Tax Rates Based on 2022 rates. 
+
+  // 18%
+  if(amount > 1 && amount < 216200) {
+    totalTaxPayment = amount * 0.18 
+    
+    }
+    // 26%
+    if(amount > 216201 && amount < 337800){
+      totalTaxPayment = amount - 216200 * 0.18 + 38916 
+    }
+    // 31%
+    if(amount > 337801 && amount < 467500){
+      totalTaxPayment = amount - 337800 * 0.31 + 70532 
+    }
+    // 36%
+    if(amount > 467501 && amount < 613600){
+      totalTaxPayment = amount - 467500 * 0.36 + 110739 
+    }
+    // 39%
+    if(amount > 613601 && amount < 782200){
+      totalTaxPayment = amount - 613600 * 0.39 + 163335 
+    }
+    // 41%
+    if(amount > 782201 && amount < 1656600){
+      totalTaxPayment = amount - 782200 * 0.41 + 229089 
+    }
+    // 45%
+    if(amount > 1656601){
+      totalTaxPayment = amount * 0.45 + 587593 
+    }
+
+  }
 
 
-
-
-console.log(totalTaxPayment);
 
 
   return (
     <div>
-      <p>You clicked {count} times</p>
+      {/* <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
-      </button>
+      </button> */}
     </div>
   );
 }
