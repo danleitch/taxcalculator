477b0da03dbd3fd6cf0cf2cb22d1d3d48407f46d
Daniel Leitch <dndleitch@gmail.com>
2021-08-13 13:34:10 +0200

    adding features

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Tax from './components/tax_logic.jsx';
 function App() {
   const [count, setCount] = useState(null);
   const [taxBracket, setTaxBracket] = useState(null);
+  const [buttonState, setButtonState] = useState(true);
+  const [totalEarnings, setTotalEarnings] = useState(null);
 
   const inputHandler = (e) => {
     const rawInputData = e.target.value;
     taxCalcs(rawInputData);
+    setTotalEarnings(rawInputData.toLocaleString('en-US'));
   };
 
   // let amount = '';
@@ -20,37 +23,37 @@ function App() {
     // 18%
     if (amount > 1 && amount < 216200) {
       totalTaxPayment = amount * 0.18;
-      setTaxBracket('18%');
+      setTaxBracket(0.18);
     }
     // 26%
     if (amount > 216201 && amount < 337800) {
-      totalTaxPayment = amount - 216200 * 0.26 + 38916;
-      setTaxBracket('26%');
+      totalTaxPayment = (amount - 216200) * 0.26 + 38916;
+      setTaxBracket(0.26);
     }
     // 31%
     if (amount > 337801 && amount < 467500) {
-      totalTaxPayment = amount - 337800 * 0.31 + 70532;
-      setTaxBracket('31%');
+      totalTaxPayment = (amount - 337800) * 0.31 + 70532;
+      setTaxBracket(0.31);
     }
     // 36%
     if (amount > 467501 && amount < 613600) {
       totalTaxPayment = amount - 467500 * 0.36 + 110739;
-      setTaxBracket('36%');
+      setTaxBracket(0.36);
     }
     // 39%
     if (amount > 613601 && amount < 782200) {
       totalTaxPayment = amount - 613600 * 0.39 + 163335;
-      setTaxBracket('39%');
+      setTaxBracket(0.39);
     }
     // 41%
     if (amount > 782201 && amount < 1656600) {
       totalTaxPayment = amount - 782200 * 0.41 + 229089;
-      setTaxBracket('41%');
+      setTaxBracket(0.41);
     }
     // 45%
     if (amount > 1656601) {
       totalTaxPayment = amount * 0.45 + 587593;
-      setTaxBracket('45%');
+      setTaxBracket(0.45);
     }
 
     if (buttonState) {
@@ -61,13 +64,19 @@ function App() {
     }
   };
 
-  let buttonState = true;
+  // let buttonState = true;
 
   const buttonHandler = (e) => {
-    buttonState = !buttonState;
-    setCount(count / 12);
-    console.log(count);
+    // buttonState = !buttonState;
+    setButtonState(!buttonState);
+    console.log(buttonState);
+    // setCount(count / 12);
+    // console.log(count);
     e.preventDefault();
+
+    if (buttonState) {
+      setCount(count * 12);
+    } else setCount(count / 12);
   };
 
   return (
@@ -76,12 +85,26 @@ function App() {
         <h1>Income Tax Calculator</h1>
       </header>
       <Tax />
+
       {!count && <h1>Enter Income and Submit </h1>}
-      {taxBracket && <h2>Your Tax Bracket is</h2>}
-      <h2>{taxBracket}</h2>
+      {taxBracket && (
+        <div>
+          <h2>
+            Your tax Bracket is {taxBracket * 100}%, tax needing to be paid per
+            a year is R{count * 12}
+          </h2>
+          <h2>
+            Your gross monthly salary is R
+            {(totalEarnings / 12).toLocaleString('en-US')} your take home
+            monthly salary is R
+            {(totalEarnings / 12 - count).toLocaleString('en-US')}
+          </h2>
+        </div>
+      )}
+
       {count && (
         <h2>
-          Tax due <br /> R{count.toFixed(2)}
+          Annual Earning is <br /> R{totalEarnings}
         </h2>
       )}
       <form>
@@ -93,7 +116,7 @@ function App() {
         />
 
         <button className="todo-button" type="submit" onClick={buttonHandler}>
-          {buttonState ? <i>Annual</i> : <i>Monthly</i>}
+          {buttonState ? <i>Switch to Annual</i> : <i>Switch to Monthly</i>}
         </button>
       </form>
     </div>
